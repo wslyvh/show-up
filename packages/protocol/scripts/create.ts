618666f2d0ffbbf64466a6d4bdbf7858540ea9d3
wslyvh <wslyvh@users.noreply.github.com>
2023-10-20 23:02:40 +0200

    update deploy scripts

--- packages/protocol/scripts/create.ts
+++ packages/protocol/scripts/create.ts
@@ -1,16 +1,23 @@
 import { ethers, network } from 'hardhat'
 import { defaultContentUri, defaultMaxParticipants, defaultTokenFee, defaultTokenMint } from '../test/utils/types'
 import { time } from '@nomicfoundation/hardhat-network-helpers'
+import dayjs from 'dayjs'
 
 export async function main() {
     console.log('Create Test data @ Show Up Protocol..')
     const [owner] = await ethers.getSigners()
 
     console.log('NETWORK ID', network.config.chainId)
-    const registry = await ethers.getContractAt('Registry', '0xfD4712Ed09b1d98354dC0153fd3eF7e374F71443')
-    const basicEtherModule = await ethers.getContractAt('BasicEther', '0xC798B6C3A99a87fE4beFb24d4599b1caA6D6DAff')
-    const basicTokenModule = await ethers.getContractAt('BasicToken', '0x317f09eCeBB6Db0d143cD7EF164F0FA1B6fC38b3')
-    const token = await ethers.getContractAt('Token', '0x3C67cE8eA670B4f180d581eC4cef79750D2d482f')
+    const registry = await ethers.getContractAt('Registry', '0x0959f7dD732631B7600fcCe67312920d4F5ECB9c')
+    const basicEtherModule = await ethers.getContractAt('BasicEther', '0x0b6b25a06A2EE6c560BB33EbDbECA831f2D67836')
+    const basicTokenModule = await ethers.getContractAt('BasicToken', '0x11FB53694e42972925940836a1E3EC71cA584936')
+    const token = await ethers.getContractAt('Token', '0x7ef7024B76791BD1f31Ac482724c76f0e24a2dD0')
+
+    // Sepolia 
+    // const registry = await ethers.getContractAt('Registry', '0xfD4712Ed09b1d98354dC0153fd3eF7e374F71443')
+    // const basicEtherModule = await ethers.getContractAt('BasicEther', '0xC798B6C3A99a87fE4beFb24d4599b1caA6D6DAff')
+    // const basicTokenModule = await ethers.getContractAt('BasicToken', '0x317f09eCeBB6Db0d143cD7EF164F0FA1B6fC38b3')
+    // const token = await ethers.getContractAt('Token', '0x3C67cE8eA670B4f180d581eC4cef79750D2d482f')
 
     // Check contracts exist
     const RegistryExists = await ethers.provider.getCode(registry.address)
@@ -36,35 +43,73 @@ export async function main() {
     const balance = await token.balanceOf(owner.address)
     console.log(owner.address, 'Balance', balance.toString())
 
+    // Create Test events
+    console.log('Create Events with BasicEther modules..')
+
+    const devconnectUri = 'ipfs://bafkreid43edjyimgerffps46bupswb5gbmkrejl3smzfrihkeidax476fy'
+    const devconnectEnd = dayjs('2023-11-19T13:00').unix()
+    const paramsEther1 = ethers.utils.defaultAbiCoder.encode(
+        ["address", 'uint256', 'uint256', 'uint256', 'address'],
+        [owner.address, devconnectEnd, ethers.utils.parseUnits('0.001', 18), defaultMaxParticipants, ethers.constants.AddressZero])
+    const tx1 = await registry.create(devconnectUri, basicEtherModule.address, paramsEther1, { gasLimit: 350000 })
+    await tx1.wait()
+
+    const ethglobalUri = 'ipfs://bafkreifphwlae7wqkui33zretrheoao22e5xkx3jijwiy6udez4jbmhbpa'
+    const ethglobalEnd = dayjs('2023-11-19T13:00').unix()
+    const paramsEther2 = ethers.utils.defaultAbiCoder.encode(
+        ["address", 'uint256', 'uint256', 'uint256', 'address'],
+        [owner.address, ethglobalEnd, ethers.utils.parseUnits('0.001', 18), defaultMaxParticipants, ethers.constants.AddressZero])
+    const tx2 = await registry.create(ethglobalUri, basicEtherModule.address, paramsEther2, { gasLimit: 350000 })
+    await tx2.wait()
+
+    const encodeUri = 'ipfs://bafkreif6ugkip3hyg6v22h2fj7h4ypziwgfonq4hleqe6tdsnfnzf7jbky'
+    const encodeEnd = dayjs('2024-07-20T13:00').unix()
+    const paramsEther3 = ethers.utils.defaultAbiCoder.encode(
+        ["address", 'uint256', 'uint256', 'uint256', 'address'],
+        [owner.address, encodeEnd, ethers.utils.parseUnits('0.001', 18), defaultMaxParticipants, ethers.constants.AddressZero])
+    const tx3 = await registry.create(encodeUri, basicEtherModule.address, paramsEther3, { gasLimit: 350000 })
+    await tx3.wait()
+
+    const supUri = 'ipfs://bafkreibljo45l7qzlv2kf26mcwykruq46pxijoi4nbecaklgaxrcl3tdk4'
+    const supEnd = dayjs('2023-10-28T13:00').unix()
+    const paramsEther4 = ethers.utils.defaultAbiCoder.encode(
+        ["address", 'uint256', 'uint256', 'uint256', 'address'],
+        [owner.address, supEnd, ethers.utils.parseUnits('0.001', 18), defaultMaxParticipants, ethers.constants.AddressZero])
+    const tx4 = await registry.create(supUri, basicEtherModule.address, paramsEther4, { gasLimit: 350000 })
+    await tx4.wait()
+
+    // Create and Cancel
+    console.log('Create and Cancel event..')
     const nextWeek = (await ethers.provider.getBlock('latest')).timestamp + time.duration.days(7)
 
-    console.log('Create Event with BasicEther condition modules..')
-    const paramsEther = ethers.utils.defaultAbiCoder.encode(
+    const cancelledEventUri = 'ipfs://bafkreiaf5svesha5s4nlvgsugfrb27rnv2k4k7wk2eqws4dw6wpfefkqsy'
+    const paramsEtherCancel = ethers.utils.defaultAbiCoder.encode(
         ["address", 'uint256', 'uint256', 'uint256', 'address'],
         [owner.address, nextWeek, ethers.utils.parseUnits('0.001', 18), defaultMaxParticipants, ethers.constants.AddressZero])
-    await registry.create(defaultContentUri, basicEtherModule.address, paramsEther, { gasLimit: 300000 })
+    const createEventTx = await registry.create(cancelledEventUri, basicEtherModule.address, paramsEtherCancel, { gasLimit: 350000 })
+    const createEventReceipt = await createEventTx.wait()
+
+    const eventCreatedEvent = createEventReceipt.events?.find((event) => event.event === 'Created')
+    if (eventCreatedEvent && eventCreatedEvent.args) {
+        await registry.cancel(eventCreatedEvent.args[0], 'Cancelled by owner', [], { gasLimit: 150000 })
+    }
 
     console.log('Create Event with BasicToken condition modules..')
     const paramsToken = ethers.utils.defaultAbiCoder.encode(
         ["address", 'uint256', 'uint256', 'uint256', 'address'],
-        [owner.address, nextWeek, defaultTokenFee, defaultMaxParticipants, token.address])
-    const createTokenTx = await registry.create(defaultContentUri, basicTokenModule.address, paramsToken, { gasLimit: 350000 })
+        [owner.address, supEnd, defaultTokenFee, defaultMaxParticipants, token.address])
+    const createTokenTx = await registry.create(supUri, basicTokenModule.address, paramsToken, { gasLimit: 400000 })
 
-    await createTokenTx.wait()
+    const createTokenReceipt = await createTokenTx.wait()
+    const createdTokenEvent = createTokenReceipt.events?.find((event) => event.event === 'Created')
+    if (createdTokenEvent && createdTokenEvent.args) {
+        console.log('Approve to spend Token..')
+        const approvalTx = await token.approve(basicTokenModule.address, defaultTokenFee);
+        await approvalTx.wait()
 
-    console.log('Approve to spend Token..')
-    const approvalTx = await token.approve(basicTokenModule.address, defaultTokenFee);
-    await approvalTx.wait()
-
-    console.log('Register for Event..')
-    await registry.register(1, owner.address, [], { gasLimit: 200000 })
-
-    console.log('Create and Cancel event..')
-    const paramsEtherCancel = ethers.utils.defaultAbiCoder.encode(
-        ["address", 'uint256', 'uint256', 'uint256', 'address'],
-        [owner.address, nextWeek, ethers.utils.parseUnits('0.001', 18), defaultMaxParticipants, ethers.constants.AddressZero])
-    await registry.create(defaultContentUri, basicEtherModule.address, paramsEtherCancel, { gasLimit: 300000 })
-    await registry.cancel(2, 'Cancelled by owner', [], { gasLimit: 100000 })
+        console.log('Register for Event..')
+        await registry.register(createdTokenEvent.args[0], owner.address, [], { gasLimit: 250000 })
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- packages/protocol/scripts/verify.ts
+++ packages/protocol/scripts/verify.ts
@@ -6,40 +6,19 @@ export async function main() {
     const [owner, attendee1, attendee2, attendee3, attendee4, attendee5] = await ethers.getSigners()
 
     console.log('NETWORK ID', network.config.chainId)
-    const Registry = await ethers.getContractFactory('Registry')
-    const registry = await Registry.deploy()
-
-    const BasicEther = await ethers.getContractFactory('BasicEther')
-    const basicEtherModule = await BasicEther.deploy()
-
-    const BasicToken = await ethers.getContractFactory('BasicToken')
-    const basicTokenModule = await BasicToken.deploy()
-
-    await registry.whitelistConditionModule(basicEtherModule.address, true)
-    await registry.whitelistConditionModule(basicTokenModule.address, true)
+    const registry = await ethers.getContractAt('Registry', '0x0959f7dD732631B7600fcCe67312920d4F5ECB9c')
+    const basicEtherModule = await ethers.getContractAt('BasicEther', '0x0b6b25a06A2EE6c560BB33EbDbECA831f2D67836')
+    const basicTokenModule = await ethers.getContractAt('BasicToken', '0x11FB53694e42972925940836a1E3EC71cA584936')
+    const token = await ethers.getContractAt('Token', '0x7ef7024B76791BD1f31Ac482724c76f0e24a2dD0')
 
     console.log('Deployment addresses:')
     console.log('Registry:', registry.address)
     console.log('BasicEther:', basicEtherModule.address)
     console.log('BasicToken:', basicTokenModule.address)
-
-    const Token = await ethers.getContractFactory('Token')
-    const token = await Token.deploy()
     console.log('Token:', token.address)
-
-    if (network.config.chainId === 31337) {
-        await token.mint(attendee1.address, defaultTokenMint)
-        await token.mint(attendee2.address, defaultTokenMint)
-        await token.mint(attendee3.address, defaultTokenMint)
-        await token.mint(attendee4.address, defaultTokenMint)
-        await token.mint(attendee5.address, defaultTokenMint)
-    }
 
     // no need to verify on localhost or hardhat
     if (network.config.chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
-        console.log(`Waiting for block confirmations..`)
-        await token.deployTransaction.wait(10) // last contract deployed
-
         console.log('Verifying Registry contract..')
         try {
             run('verify:verify', {
