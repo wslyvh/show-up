7ade315284e18222b5d641bf1931a65efcc58ec9
wslyvh <wslyvh@users.noreply.github.com>
2023-11-24 11:41:21 +0100

    walletconnect fix

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
   images: {
