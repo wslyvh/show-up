051486d2898a507f199396bfed141e7f4e255d74
wslyvh <wslyvh@users.noreply.github.com>
2023-12-09 14:28:13 +0100

    page timeout

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -5,6 +5,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 180,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
