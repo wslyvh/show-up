60027c963e3043397ca0797a9215071460db97bd
wslyvh <wslyvh@users.noreply.github.com>
2023-12-14 10:23:34 +0100

    update footer

--- packages/blog/src/components/Footer.tsx
+++ packages/blog/src/components/Footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { SITE_DESCRIPTION, SOCIAL_GITHUB, SOCIAL_TWITTER } from 'app/src/utils/site'
+import { SITE_INFO, SOCIAL_GITHUB, SOCIAL_TWITTER } from 'app/src/utils/site'
 import { FaGithub, FaTwitter } from 'react-icons/fa6'
 import { LinkComponent } from 'app/src/components/LinkComponent'
 
 export function Footer() {
     return (
         <footer className='footer flex justify-between items-center p-4'>
-            <p>{SITE_DESCRIPTION}</p>
+            <p>{SITE_INFO}</p>
             <div className='flex gap-4'>
                 <LinkComponent href={`https://github.com/${SOCIAL_GITHUB}`}>
                     <FaGithub />
