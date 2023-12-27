ecf6d578be734c61a9389ad9f79820e02bfab7ca
wslyvh <wslyvh@users.noreply.github.com>
2023-12-27 15:23:51 +0100

    v1

--- packages/app/tailwind.config.ts
+++ packages/app/tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   theme: {},
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     // More details at https://daisyui.com/docs/config/
